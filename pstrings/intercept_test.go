13824d16103ffb490267dc9f2977a89ce8ca1089
pantianying <601666418@qq.com>
2019-11-07 14:17:59 +0800

    update

--- pstrings/intercept_test.go
+++ pstrings/intercept_test.go
@@ -12,27 +12,23 @@ func TestIntercept(t *testing.T) {
 	assert.Equal(t, result["uid"], "1")
 }
 func TestIntercept1(t *testing.T) {
-	pattern := "/user/pan1/sayHello/{uid}/"
-	url := "/user/pan1/sayHello/1/"
+	pattern := "/user/pan1/sayHello/{uid}"
+	url := "/user/pan1/sayHello/1"
 	result, _ := Intercept(pattern, url, "{", "}")
 	assert.Equal(t, result["uid"], "1")
 }
-func TestIntercept2(t *testing.T) {
-	pattern := "xxxx{uid}xxxx{aaa}aaa{bbb}ccc"
-	url := "xxxxx23xxxxcccaaaBBBccc"
-	result, _ := Intercept(pattern, url, "{", "}")
-	assert.Equal(t, result["uid"], "x23")
-}
 func TestInterceptV2_1(t *testing.T) {
-	pattern := "/user/pan1/sayHello/{uid}"
+	pattern := "GET:/user/pan1/sayHello/{uid}"
 	url := "/user/pan1/sayHello/1"
 	result, err := InterceptV2(pattern, url, "{", "}")
 	assert.Equal(t, nil, err)
 	assert.Equal(t, "1", result["uid"])
 }
 func TestInterceptV2_2(t *testing.T) {
-	pattern := "/user/pan1/sayHello/{uid}/"
-	url := "/user/pan1/sayHello/1/xxx"
-	result, _ := InterceptV2(pattern, url, "{", "}")
-	assert.Equal(t, result["uid"], "1")
+	pattern := "GET:/{user}/pan1/sayHello/{uid}/"
+	url := "xxx/pan/pan1/sayHello/1/"
+	result, err := InterceptV2(pattern, url, "{", "}")
+	assert.Equal(t, nil, err)
+	assert.Equal(t, "1", result["uid"])
+	assert.Equal(t, "pan", result["user"])
 }
